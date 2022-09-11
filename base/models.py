8e78d161f9bfd89f14a924d766e1d7abf78602b1
Vladislav Korecky <vladislav@korecky.org>
2022-09-11 13:54:41 +0200

    Založen kompletní ORM a vytvořena stránka na hledání

--- base/models.py
+++ base/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Room(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    participants = models.ManyToManyField(User, related_name='participants', blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['-created', 'name', '-updated']
+
+
+class Message(models.Model):
+    body = models.TextField()
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.body[0:50]
+
+    class Meta:
+        ordering = ['-created', '-updated']
+
--- base/views.py
+++ base/views.py
@@ -1,8 +1,23 @@
+from django.db.models import Q
 from django.http import HttpResponse
 from django.shortcuts import render
+
+from base.models import Room
 
 
 # Create your views here.
 def hello(request):
     s = request.GET.get('s', '')
     return HttpResponse(f'Hello, {s} world!')
+
+
+def search(request):
+    q = request.GET.get('q', '')  # 127.0.0.1/search?q=Dja
+    if q == '':
+        return HttpResponse("Prosím zadejte hledaný výraz.")
+    rooms = Room.objects.filter(
+        Q(description__contains=q) |
+        Q(name__contains=q)
+    )
+    context = {'query': q, 'rooms': rooms}
+    return render(request, "base/search.html", context)
